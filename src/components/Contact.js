90824594e7e29dc9795dacf244c36a6fbaf030f1
V-gy <vgy@hotmail.fr>
2019-10-15 13:53:34 +0200

    quete react 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,37 @@
 import React from 'react'
-function Contacts(props){
+
+class Contacts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {online:false}
+    }
+    render(){
     return(
         <figure className="Contact">
             
             <img className = "avatar"
-                src={props.user.avatar}
-                alt={props.user.name}/>
-            <div>
-                <h2 className = "name"> {props.user.name} </h2>
-                <div className = "status">
-                {props.user.status?<p className = "status-online"></p>:<p className = "status-offline "></p> }
-                {props.user.status?<p className ="status-text">online</p>:<p className = "status-text">offline</p>}
-                
-                </div>            
+                src={this.props.user.avatar}
+                alt={this.props.user.name}/>
+            <div 
+                    >
+                <h2 className = "name"> {this.props.user.name} </h2>
+                <div className= "status" onClick = {event=> {
+                        let newOnline = !this.state.online;
+                        this.setState({ online: newOnline});}}>
+                    <span className = 
+                    {this.state.online ? "status-online": "status-offline"}>    
+                    </span>
+                    <span> {this.state.online ? 
+                    <p className= "status-text">Online</p> : 
+                    <p className= "status-text"> Offline</p> }
+                    </span>
+              </div>  
+            
+                          
             </div>
         </figure>
     )
+}
 }
 
 export default Contacts
